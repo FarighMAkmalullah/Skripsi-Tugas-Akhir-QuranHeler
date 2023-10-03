ecadfcccd8a62feaeb159ca7f534fc043e2549ad
FarighMAkmalullah <48850497+FarighMAkmalullah@users.noreply.github.com>
2023-10-03 19:27:13 +0700

    Buat Service Adzan

---
+++ lib/services/adzan/adzan_service.dart
@@ -0,0 +1,24 @@
+import 'package:quranhealer/core/init/const/api.dart';
+import 'package:dio/dio.dart';
+
+class AdzanService {
+  final _dio = Dio();
+
+  Future<List<Map<String, dynamic>>> fetchAdzanData() async {
+    try {
+      final response = await _dio.get("$adzanAPI/sholat/format/json/kota");
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = response.data['data'];
+        return data
+            .map<Map<String, dynamic>>(
+                (item) => Map<String, dynamic>.from(item))
+            .toList();
+      } else {
+        throw Exception('Failed to load Adzan List');
+      }
+    } catch (error) {
+      throw Exception('Error: $error');
+    }
+  }
+}
