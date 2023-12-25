b30794ed791c325e249d4ee184d9068575c6ab80
Dodenpernando <147404307+Dodenpernando@users.noreply.github.com>
2023-12-25 21:42:18 +0700

    masukin ext terms and policy

--- lib/screens/terms_and_privacy_policy/terms_screen.dart
+++ lib/screens/terms_and_privacy_policy/terms_screen.dart
@@ -5,6 +5,25 @@ class TermsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Terms and Conditions (Syarat dan Ketentuan'),
+          Text('Selamat datang di QuranHealer, sebuah aplikasi yang dirancang untuk membantu Anda dalam menjalani kehidupan yang lebih baik dan penuh makna melalui penggunaan fitur-fitur berikut:'),
+          Text("1. Penggunaan Aplikasi"),
+          Text("a. Dengan mengunduh dan menggunakan aplikasi QuranHealer, Anda setuju untuk mematuhi semua ketentuan dan persyaratan dalam Syarat dan Ketentuan ini."),
+          Text("Anda bertanggung jawab penuh atas penggunaan aplikasi ini, dan Anda tidak akan menggunakan aplikasi ini untuk tujuan yang melanggar hukum atau merugikan pihak lain."),
+          Text("2. Autentikasi"),
+          Text("a. Untuk menggunakan fitur-fitur tertentu dalam aplikasi, Anda harus membuat akun pengguna. Data pribadi yang Anda berikan saat pendaftaran akan dijaga sesuai dengan Kebijakan Privasi kami."),
+          Text("b. Anda bertanggung jawab untuk menjaga kerahasiaan informasi login Anda dan tidak boleh membagikannya kepada pihak lain."),
+          Text("3. Jadwal Adzan"),
+          Text("a. Fitur jadwal adzan disediakan sebagai referensi dan panduan untuk waktu ibadah. Kami tidak bertanggung jawab atas ketidakakuratan atau perubahan jadwal yang mungkin terjadi."),
+          Text("4. Al-Quran dan Doa Pilihan"),
+          Text("a. Aplikasi ini menyediakan akses ke Al-Quran dan berbagai doa pilihan. Pengguna bertanggung jawab atas penggunaan materi ini dan diharapkan untuk mematuhi aturan dan etika yang sesuai."),
+          Text("5. Hak Cipta"),
+          Text("a. Semua konten dalam aplikasi, termasuk teks Al-Quran dan doa-doa, adalah hak cipta dilindungi oleh undang-undang. Pengguna tidak diperkenankan untuk menggandakan, mendistribusikan, atau mempublikasikan konten tersebut tanpa izin."),
+        ],
+      ),
+    );
   }
 }
